974544aa1bd28410cfcf109a71a4e8d4d54156bc
Jamie Kelsch <jamiedk@gmail.com>
2019-11-16 21:40:32 -0700

    Test stuff

--- hw3d/Chili/WinMain.cpp
+++ hw3d/Chili/WinMain.cpp
@@ -1,4 +1,5 @@
 #include "Window.h"
+#include <sstream>
 
 
 
@@ -18,6 +19,26 @@ int WINAPI WinMain(
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+
+			while (!wnd.mouse.IsEmpty())
+			{
+				const auto e = wnd.mouse.Read();
+				switch (e.GetType())
+				{
+					case Mouse::Event::Type::Leave:
+					{
+						wnd.SetTitle("Gone!");
+						break;
+					}
+					case Mouse::Event::Type::Move:
+					{
+						std::ostringstream oss;
+						oss << "Mouse position: (" << e.GetPosX() << ", " << e.GetPosY() << ")";
+						wnd.SetTitle(oss.str());
+						break;
+					}
+				}
+			}
 		}
 
 		if (gResult == -1)
