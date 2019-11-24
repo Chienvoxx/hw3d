da7ad4e745cbeb2c7174b601c83e9bc421fafe24
Jamie Kelsch <jamiedk@gmail.com>
2019-11-23 23:06:41 -0700

    DXGI debug info added

--- hw3d/Chili/Graphics.cpp
+++ hw3d/Chili/Graphics.cpp
@@ -4,8 +4,21 @@
 
 #pragma comment(lib, "d3d11.lib")
 
-#define GFX_THROW_FAILED(hrcall) if(FAILED( hr = (hrcall) ) ) throw Graphics::HrException(__LINE__, __FILE__, hr)
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, hr)
+//#define GFX_THROW_FAILED(hrcall) if(FAILED( hr = (hrcall) ) ) throw Graphics::HrException(__LINE__, __FILE__, hr)
+// graphic exception checking/throwing macros (some with dxgi infos)
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException(__LINE__, __FILE__, hr)
+#define GFX_THROW_NOINFO(hrcall) if(FAILED( hr = (hrcall) ) ) throw Graphics::HrException(__LINE__, __FILE__, hr)
+
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#define GFX_THROW_INFO(hrcall) infoManager.Set(); if(FAILED(hr = (hrcall)) ) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#else
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr))
+#endif
+
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -26,15 +39,20 @@ Graphics::Graphics(HWND hWnd)
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	sd.Flags = 0;
 
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
 	// for checking results of d3d functions
 	HRESULT hr;
 
 	// create device and front/back buffers, and swap chain, and rendering target
-	GFX_THROW_FAILED(D3D11CreateDeviceAndSwapChain(
+	GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
-		0,
+		swapCreateFlags,
 		nullptr,
 		0,
 		D3D11_SDK_VERSION,
@@ -46,8 +64,8 @@ Graphics::Graphics(HWND hWnd)
 	) );
 	// gain access to texture subresource in swap chain (back buffer)
 	ID3D11Resource* pBackBuffer = nullptr;
-	GFX_THROW_FAILED(pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer)));
-	GFX_THROW_FAILED(pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pTarget));
+	GFX_THROW_INFO(pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer)));
+	GFX_THROW_INFO(pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pTarget));
 	pBackBuffer->Release();
 }
 
@@ -79,7 +97,7 @@ void Graphics::EndFrame()
 		}
 		else
 		{
-			GFX_THROW_FAILED(hr);
+			throw GFX_EXCEPT(hr);
 		}
 	}
 }
@@ -91,9 +109,21 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 }
 
 // Graphics exception stuff
-Graphics::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+Graphics::HrException::HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
 	:Exception(line, file), hr(hr)
-{}
+{
+	// join all info messages with newlines into single string
+	for (const auto& m : infoMsgs)
+	{
+		info += m;
+		info.push_back('\n');
+	}
+	//remove final newline if exists
+	if (!info.empty())
+	{
+		info.pop_back();
+	}
+}
 
 const char* Graphics::HrException::what() const noexcept
 {
@@ -102,8 +132,12 @@ const char* Graphics::HrException::what() const noexcept
 		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
 		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
 		<< "[Error String] " << GetErrorString() << std::endl
-		<< "[Description] " << GetErrorDescription() << std::endl
-		<< GetOriginString();
+		<< "[Description] " << GetErrorDescription() << std::endl;
+	if (info.empty())
+	{
+		oss << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
+	}	
+	oss << GetOriginString();
 	whatBuffer = oss.str();
 	return whatBuffer.c_str();
 }
@@ -128,6 +162,11 @@ std::string Graphics::HrException::GetErrorDescription() const noexcept
 	char buff[512];
 	DXGetErrorDescription(hr, buff, sizeof(buff));
 	return buff;
+}
+
+std::string Graphics::HrException::GetErrorInfo() const noexcept
+{
+	return info;
 }
 
 const char* Graphics::DeviceRemovedException::GetType() const noexcept
--- hw3d/Chili/Graphics.h
+++ hw3d/Chili/Graphics.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "ChiliWin.h"
 #include "ChiliException.h"
+#include "DxgiInfoManager.h"
 #include <d3d11.h>
+#include <vector>
 
 
 class Graphics
@@ -15,20 +17,24 @@ public:
 	class HrException : public Exception
 	{
 	public:
-		HrException(int line, const char* file, HRESULT hr) noexcept;
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsg = {}) noexcept;
 		const char* what() const noexcept override;
 		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
 		std::string GetErrorString() const noexcept;
 		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
 	private:
 		HRESULT hr;
+		std::string info;
 	};
 	class DeviceRemovedException : public HrException
 	{
 		using HrException::HrException;
 	public:
 		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
 	};
 public:
 	Graphics(HWND hWnd);
@@ -38,6 +44,9 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device*			pDevice		= nullptr;
 	IDXGISwapChain*			pSwap		= nullptr;
 	ID3D11DeviceContext*	pContext	= nullptr;
