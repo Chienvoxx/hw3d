7e06347bb50e2aa01364f6cc4d45c8e3d43fe08d
Jamie Kelsch <jamiedk@gmail.com>
2019-11-23 21:29:11 -0700

    D3D HRESULT handling / Window nogfx

--- hw3d/Chili/Window.cpp
+++ hw3d/Chili/Window.cpp
@@ -103,7 +103,7 @@ void Window::SetTitle(const std::string& title)
 	}
 }
 
-std::optional<int> Window::ProcessMessages()
+std::optional<int> Window::ProcessMessages() noexcept
 {
 	MSG msg; // the tagMSG struct
 	// while queue has messages, remove and dispatch them (but do not block on empty queue)
@@ -125,6 +125,10 @@ std::optional<int> Window::ProcessMessages()
 
 Graphics& Window::Gfx()
 {
+	if (!pGfx)
+	{
+		throw CHWND_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -293,34 +297,34 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 }
 
 
-Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
-	: ChiliException(line, file), hr(hr)
-{}
-
-
-const char* Window::Exception::what() const noexcept
-{
-	std::ostringstream oss;
-	oss << GetType() << std::endl
-		<< "[Error Code] " << GetErrorCode() << std::endl
-		<< "[Description] " << GetErrorString() << std::endl
-		<< GetOriginString();
-	whatBuffer = oss.str();
-	return whatBuffer.c_str();
-}
-
-
-const char* Window::Exception::GetType() const noexcept
-{
-	return "Chili Window Exception";
-}
+//Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
+//	: ChiliException(line, file), hr(hr)
+//{}
+//
+//
+//const char* Window::Exception::what() const noexcept
+//{
+//	std::ostringstream oss;
+//	oss << GetType() << std::endl
+//		<< "[Error Code] " << GetErrorCode() << std::endl
+//		<< "[Description] " << GetErrorString() << std::endl
+//		<< GetOriginString();
+//	whatBuffer = oss.str();
+//	return whatBuffer.c_str();
+//}
+//
+//
+//const char* Window::Exception::GetType() const noexcept
+//{
+//	return "Chili Window Exception";
+//}
 
 
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
 	char* pMsgBuf = nullptr;
 	// windows will allocate memory for err string and make our pointer point to it
-	DWORD nMsgLen = FormatMessage(
+	const DWORD nMsgLen = FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER |
 		FORMAT_MESSAGE_FROM_SYSTEM |
 		FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -338,14 +342,38 @@ std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 	return errorString;
 }
 
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	: Exception(line, file), hr(hr)
+{}
 
-HRESULT Window::Exception::GetErrorCode() const noexcept
+const char* Window::HrException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Window::HrException::GetType() const noexcept
+{
+	return "Chili Window Exception";
+}
+
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-
-std::string Window::Exception::GetErrorString() const noexcept
+std::string Window::HrException::GetErrorDescription() const noexcept
 {
-	return TranslateErrorCode(hr);
+	return Exception::TranslateErrorCode(hr);
+}
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "Chili Window Exception [No Graphics]";
 }
--- hw3d/Chili/Window.h
+++ hw3d/Chili/Window.h
@@ -9,23 +9,36 @@
 
 
 // Error exception helper macro
-#define CHWND_EXCEPT(hr) Window::Exception(__LINE__, __FILE__, hr)
-#define CHWND_LAST_EXCEPT() Window::Exception(__LINE__, __FILE__, GetLastError())
+#define CHWND_EXCEPT(hr) Window::HrException(__LINE__, __FILE__, hr)
+#define CHWND_LAST_EXCEPT() Window::HrException(__LINE__, __FILE__, GetLastError())
+#define CHWND_NOGFX_EXCEPT() Window::NoGfxException(__LINE__, __FILE__)
 
 class  Window
 {
 public:
 	class Exception : public ChiliException
 	{
+		using ChiliException::ChiliException;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		//static std::string TranslateErrorCode(HRESULT hr) noexcept;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
+	};
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
 	};
 
 private:
@@ -50,7 +63,7 @@ public:
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
 	void SetTitle(const std::string& title);
-	static std::optional<int> ProcessMessages();
+	static std::optional<int> ProcessMessages() noexcept;
 	Graphics& Gfx();
 public:
 	Keyboard kbd;
