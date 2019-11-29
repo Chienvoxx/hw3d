ce5b980fa2f10b05004a7c5eb256589f537dc745
Jamie Kelsch <jamiedk@gmail.com>
2019-11-29 00:12:45 -0700

    Test mouse optional event sys

--- hw3d/Chili/App.cpp
+++ hw3d/Chili/App.cpp
@@ -1,4 +1,5 @@
 #include "App.h"
+#include <sstream>
 
 
 
@@ -23,6 +24,19 @@ int App::Go()
 
 void App::DoFrame()
 {
+	while (const auto e = wnd.mouse.Read())
+	{
+		switch (e->GetType())
+		{
+		case Mouse::Event::Type::Move:
+		{
+			std::ostringstream oss;
+			oss << "Mouse moved to: (" << e->GetPosX() << ", " << e->GetPosY() << ")\n";
+			wnd.SetTitle(oss.str());
+			break;
+		}
+		}
+	}
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
 	wnd.Gfx().ClearBuffer(c, c, c);
 	wnd.Gfx().DrawTestTriangle();
