a8ce8b832c24e66229fadaa37ee7f384c514e587
Jamie Kelsch <jamiedk@gmail.com>
2019-11-30 01:21:13 -0700

    Mouse translation DXMath

--- hw3d/Chili/App.cpp
+++ hw3d/Chili/App.cpp
@@ -25,6 +25,9 @@ void App::DoFrame()
 {
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
 	wnd.Gfx().ClearBuffer(c, c, c);
-	wnd.Gfx().DrawTestTriangle(timer.Peek());
+	wnd.Gfx().DrawTestTriangle(
+		timer.Peek(),
+		wnd.mouse.GetPosX() / 400.0f - 1.0f,
+		-wnd.mouse.GetPosY() / 300.0f + 1.0f);
 	wnd.Gfx().EndFrame();
 }
