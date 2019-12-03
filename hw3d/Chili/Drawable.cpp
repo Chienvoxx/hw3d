714351285c17f641013284be94f40cd6150dbae2
Jamie Kelsch <jamiedk@gmail.com>
2019-12-03 08:00:30 -0700

    stStic bindables (crashes, but try 1 box only)

--- hw3d/Chili/Drawable.cpp
+++ hw3d/Chili/Drawable.cpp
@@ -11,6 +11,10 @@ void Drawable::Draw(Graphics& gfx) const noexcept(!IS_DEBUG)
 	{
 		b->Bind(gfx);
 	}
+	for (auto& b : GetStaticBinds())
+	{
+		b->Bind(gfx);
+	}
 	gfx.DrawIndexed(pIndexBuffer->GetCount());
 }
 
@@ -20,10 +24,9 @@ void Drawable::AddBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
 	binds.push_back(std::move(bind));
 }
 
-void Drawable::AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept
+void Drawable::AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept(!IS_DEBUG)
 {
 	assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
 	pIndexBuffer = ibuf.get();
 	binds.push_back(std::move(ibuf));
 }
-
