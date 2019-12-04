9089367a7ef31a179a0bb6f059d4d847c2962141
Jamie Kelsch <jamiedk@gmail.com>
2019-12-03 21:24:18 -0700

    Static vertex constant buffer + protected:tion

--- hw3d/Chili/TransformCbuf.h
+++ hw3d/Chili/TransformCbuf.h
@@ -10,7 +10,7 @@ public:
 	TransformCbuf(Graphics& gfx, const Drawable& parent);
 	void Bind(Graphics& gfx) noexcept override;
 private:
-	VertexConstantBuffer<DirectX::XMMATRIX> vcbuf;
+	static std::unique_ptr<VertexConstantBuffer<DirectX::XMMATRIX>> pVcbuf;
 	const Drawable& parent;
 };
 
